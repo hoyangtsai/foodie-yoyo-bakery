72de7e3018740f18f7d3e1ec157ae0ead49a95d5
Ho-Yang Tsai <hoyang.t@gmail.com>
2017-06-26 00:02:09 +0800

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,26 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
 };
